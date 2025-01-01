5730d3b0296e997f1ae094f08bdd34937ee2c241
pranay-7671 <katragaddapranay@gmail.com>
2025-01-01 12:34:47 +0530

    Add files via upload

---
+++ Hospital-mangement-main/hosptial_application/frontend/scroll_function.js
@@ -0,0 +1,12 @@
+let slideIndex = 0;
+        slide();
+        function slide() {
+            let slides=document.getElementsByClassName("mySlides");
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        slides[slideIndex-1].style.display = "block"; 
+        setTimeout(slide,2000);
+    }
